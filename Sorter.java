2a9b125500c3b1513ab259d08f3fe8af421762fc
zainab <zahm1088@gmail.com>
2024-02-11 21:15:11 +0300

    selectionSort method

--- Sorter.java
+++ Sorter.java
@@ -1,2 +1,22 @@
 public class Sorter {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // One by one move the boundary of the unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
